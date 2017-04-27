36f4c350f889ec4ee0e30b8eceb4e36aeaa506e4
Philip Axelrod <philip.axelrod.web@gmail.com>
2017-04-26 17:01:20 -0700

    Attempt  and fail to fix null pointer exceptions in histogram apply method

--- commons/src/main/scala/com/lynbrookrobotics/potassium/commons/Histogram.scala
+++ commons/src/main/scala/com/lynbrookrobotics/potassium/commons/Histogram.scala
@@ -1,16 +1,31 @@
 package com.lynbrookrobotics.potassium.commons
 
-/**
-  * Created by the-magical-llamicorn on 4/19/17.
-  */
+
 class Histogram(min: Double, max: Double, bins: Int) {
   val histogram = Array[Long](bins + 2)
   val interval = (max - min) / bins
+  clear()
 
   def apply(value: Double): Unit = {
-    if (value < min) histogram(0) += 1
-    else if (value > max) histogram(bins + 1) += 1
-    else histogram(1 + ((value - min) / interval).toInt) += 1
+    println("in apply!!!!!!!!!")
+    println(s"value $value ")
+    println(s"min: $min")
+    if (value < min) {
+      println("value < min")
+      histogram(0) += 1
+    }
+    else if (value > max){
+      println("value > min")
+      histogram(bins + 1) += 1
+    }
+    else {
+      println("else")
+      histogram(1 + ((value - min) / interval).toInt) += 1
+    }
+  }
+
+  def clear(): Unit = {
+    histogram.map(_ => 0)
   }
 
   override def toString(): String = {
